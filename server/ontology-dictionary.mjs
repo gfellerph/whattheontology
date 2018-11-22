b01b5ab623c7ebfd5893d51a69d57e2f86b41f14
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-11-22 17:54:57 +0700

    Add some more ontologies

--- server/ontology-dictionary.mjs
+++ server/ontology-dictionary.mjs
@@ -2,7 +2,7 @@ export default {
   // Spec reference, as found in 'why.value' in quad statements
   'https://www.w3.org/ns/solid/terms': {
     // Nickname for the spec
-    title: 'Solid Terms',
+    title: 'SOLID_TERMS',
     // The parsable spec document (parser: rdflib)
     resource: 'https://www.w3.org/ns/solid/terms#',
     // More infos, web reference, user will be redirected here on click
@@ -17,11 +17,11 @@ export default {
     resource: 'http://www.w3.org/ns/pim/space#',
   },
   'http://purl.org/dc/terms/': {
-    title: 'DC Terms',
+    title: 'DC_TERMS',
     resource: 'http://purl.org/dc/terms/',
   },
   'http://purl.org/dc/elements/1.1/': {
-    title: 'DC Elements',
+    title: 'DC_ELEMENTS',
     resource: 'http://purl.org/dc/elements/1.1/',
   },
   'http://www.w3.org/2000/01/rdf-schema': {
@@ -45,7 +45,7 @@ export default {
     resource: 'https://www.w3.org/1999/02/22-rdf-syntax-ns#',
   },
   'https://www.w3.org/ns/pim/arg': {
-    title: 'PIM Argument',
+    title: 'PIM_ARGUMENT',
     resource: 'https://www.w3.org/ns/pim/arg#',
   },
   'http://www.w3.org/2002/12/cal/ical': {
@@ -53,7 +53,7 @@ export default {
     resource: 'http://www.w3.org/2002/12/cal/ical#',
   },
   'http://www.w3.org/2000/10/swap/pim/contact': {
-    title: 'PIM Contact',
+    title: 'PIM_CONTACT',
     resource: 'http://www.w3.org/2000/10/swap/pim/contact#',
   },
   'http://usefulinc.com/ns/doap': {
@@ -63,6 +63,74 @@ export default {
   'https://www.w3.org/2002/07/owl': {
     title: 'OWL',
     resource: 'https://www.w3.org/2002/07/owl#',
+  },
+  'http://www.w3.org/2007/ont/http': {
+    title: 'HTTP',
+    resource: 'http://www.w3.org/2007/ont/http#',
+  },
+  'https://www.w3.org/2007/ont/httph': {
+    title: 'HTTPH',
+    resource: 'https://www.w3.org/2007/ont/httph#',
+  },
+  'http://www.w3.org/2002/12/cal/icaltzd': {
+    title: 'ICAL_TZD',
+    resource: 'http://www.w3.org/2002/12/cal/icaltzd#',
+  },
+  'http://www.w3.org/ns/ldp': {
+    title: 'LDP',
+    resource: 'http://www.w3.org/ns/ldp#',
+  },
+  'http://www.w3.org/2007/ont/link': {
+    title: 'LINK',
+    resource: 'http://www.w3.org/2007/ont/link#',
+  },
+  'http://www.w3.org/2000/10/swap/log': {
+    title: 'LOG',
+    resource: 'http://www.w3.org/2000/10/swap/log#',
+  },
+  'https://www.w3.org/ns/pim/meeting': {
+    title: 'MEETING',
+    resource: 'http://www.w3.org/ns/pim/meeting#',
+  },
+  'http://purl.org/ontology/mo/': {
+    title: 'MO',
+    resource: 'http://purl.org/ontology/mo/',
+  },
+  'http://www.w3.org/2000/10/swap/pim/qif': {
+    title: 'QIF',
+    resource: 'http://www.w3.org/2000/10/swap/pim/qif#',
+  },
+  'http://www.w3.org/ns/pim/trip': {
+    title: 'TRIP',
+    resource: 'http://www.w3.org/ns/pim/trip#',
+  },
+  'http://purl.org/rss/1.0/': {
+    title: 'RSS',
+    resource: 'http://purl.org/rss/1.0/',
+  },
+  'https://www.w3.org/ns/pim/schedule': {
+    title: 'PIM_SCHEDULE',
+    resource: 'https://www.w3.org/ns/pim/schedule#',
+  },
+  'http://www.w3.org/ns/pim/space': {
+    title: 'PIM_SPACE',
+    resource: 'http://www.w3.org/ns/pim/space#',
+  },
+  'http://www.w3.org/ns/posix/stat': {
+    title: 'STAT',
+    resource: 'http://www.w3.org/ns/posix/stat#',
+  },
+  'http://www.w3.org/ns/ui#': {
+    title: 'UI',
+    resource: 'http://www.w3.org/ns/ui#',
+  },
+  'http://www.w3.org/2005/01/wf/flow': {
+    title: 'FLOW',
+    resource: 'http://www.w3.org/2005/01/wf/flow#',
+  },
+  'http://www.w3.org/2001/XMLSchema#': {
+    title: 'XML',
+    resource: 'https://www.w3.org/2009/XMLSchema/XMLSchema.xsd',
   },
   /* 'whatever': {
     title: 'Error',
