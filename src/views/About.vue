13e8fef5b608e361200f156a146769565346a355
Philipp Gfeller <philipp.gfeller@maxomedia.ch>
2018-11-22 12:17:23 +0700

    Add about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+  div.about.container
+    h2 What the ontology
+    p A search engine for RDF namespaces, properties, classes, ontologies and datatypes recommended by the <a href="https://solid.mit.edu/">SOLID project</a>.
+    ul
+      li(
+        v-for="(ont, i) in orderedDict"
+        :key="i"
+      )
+        a(:href="ont.resource") {{ont.title}}
+        span.lightgrey  ({{ont.resource}})
 </template>
+
+<script>
+import dict from '@/../server/ontology-dictionary.mjs';
+
+export default {
+  data() {
+    return {
+      dict,
+    }
+  },
+  computed: {
+    orderedDict() {
+      return Object.values(this.dict).sort((a, b) => {
+        return a.title.toUpperCase() < b.title.toUpperCase() ? -1 : 1;
+      });
+    }
+  }
+}
+</script>
