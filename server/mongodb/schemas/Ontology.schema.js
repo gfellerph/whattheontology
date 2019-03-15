96e8f7621dd474ffed19422b52a7b9d3cd6d9ebe
Philipp Gfeller <tuelsch@gmail.com>
2019-03-15 20:35:21 +0800

    Save some more stats

--- server/mongodb/schemas/Ontology.schema.js
+++ server/mongodb/schemas/Ontology.schema.js
@@ -5,25 +5,36 @@ const Schema = mongoose.Schema;
 const Ontology = new Schema({
   url: String,
   name: String,
+  homepage: String,
+  prefix: String,
   error: Object,
+  indexed: {
+    type: Boolean,
+    default: false,
+  },
 }, {
   timestamps: true,
 });
 
 Ontology.virtual('setError').set(function (value) {
-  this.error = serializeError(value);
+  if (!value) {
+    this.error = null;
+  } else {
+    this.error = serializeError(value);
+  }
+  this.markModified('error');
 });
 
-Ontology.methods.findErrors = async function () {
-  return this.model.find({ error: { $exists: true }});
+Ontology.statics.findErrors = async function () {
+  return this.find({ error: { $exists: true, $ne: null }}).lean();
 }
 
-Ontology.methods.findIndexed = async function () {
-  return this.model.find({ error: { $exists: false }});
+Ontology.statics.findIndexed = async function () {
+  return this.find({ error: null, indexed: true }).lean();
 }
 
-Ontology.methods.exists = async function () {
-  return this.model.find({ url: this.url });
+Ontology.statics.exists = async function () {
+  return this.find({ url: this.url });
 }
 
 module.exports = mongoose.model('Ontology', Ontology);
